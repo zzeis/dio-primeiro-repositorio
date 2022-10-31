95086e1cfdac7adbbf65c0929ec8ed30637bd5f5
zzeis <moisesmonteiro.stark@gmail.com>
2022-10-31 16:08:05 -0300

    Adicionando pasta tipos-variaveis

--- JAVA/java-anatomia-classes/src/edu/zeis/primeirosEX/BoletimEstudantil.java
+++ JAVA/java-anatomia-classes/src/edu/zeis/primeirosEX/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.zeis.primeirosEX;
 
 public class BoletimEstudantil {
     public static void main(String[] args) {
--- JAVA/java-anatomia-classes/src/edu/zeis/primeirosEX/MinhaClasse.java
+++ JAVA/java-anatomia-classes/src/edu/zeis/primeirosEX/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.zeis.primeirosEX;
 public class MinhaClasse {
 
     public static void main(String[] args) {
