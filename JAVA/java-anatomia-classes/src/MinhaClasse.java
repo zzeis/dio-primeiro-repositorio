57f65f45d7371a65f8ccff2a526097071c41241c
zzeis <moisesmonteiro.stark@gmail.com>
2022-10-31 15:07:18 -0300

    Adicionado Classe BoletimEstudantil

--- JAVA/java-anatomia-classes/src/MinhaClasse.java
+++ JAVA/java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Hello world! ");
+        String primeiroNome = "Moises";
+        String segundoNome = "Monteiro";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
