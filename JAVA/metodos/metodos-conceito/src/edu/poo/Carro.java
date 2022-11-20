fc4c5701f9e0bb8560d13e57d6dadbd53d59dcb5
zzeis <moisesmonteiro.stark@gmail.com>
2022-11-20 12:41:46 -0300

    Adicionado a classe Carro

---
+++ JAVA/metodos/metodos-conceito/src/edu/poo/Carro.java
@@ -0,0 +1,48 @@
+package edu.poo;
+
+public class Carro {
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+
+    Carro(){ //constructor 
+
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){ //sobrecarga
+        this.cor = cor; 
+        this.modelo = modelo; 
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
+
+}
